18066de16c70b8ff8105ff612ec81d0774c1a77b
pss940909 <pss940909@gmail.com>
2023-03-25 20:21:39 +0800

    Revert "push chart"
    This reverts commit 18306c0a80f81cb3bc73d7ebe90d738319504720.

--- vue-lessons/src/App.vue
+++ vue-lessons/src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/chart">後台圖表</router-link> |
-    <router-link to="/member">會員中心</router-link>
+    <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
 </template>
