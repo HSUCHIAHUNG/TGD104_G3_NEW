1eacf3c2fd06d65ae428077901ddb7f78081ba85
Swimming0319 <stitch2007319@gmail.com>
2023-03-29 18:19:25 +0800

    travel &travel_map

---
+++ vue-lessons/src/views/SelTravelConsultant.vue
@@ -0,0 +1,113 @@
+<template>
+    <!-- sel_travel_consultant_banner -->
+    <div class="sel_travel_consultant_banner">
+      <div class="container">
+        <h1>
+          不孤單的旅行，<br>
+          就從有<span>陪你</span>開始。
+        </h1>
+        <h2>喜歡登山？喜歡單車？喜歡潛水？</h2>
+        <h3>就讓<span>陪你</span>成為你的好旅伴，一起來場無限可能的旅行吧！</h3>
+        <h4>開始預約<span>陪你</span>顧問</h4>
+        <i class="fa-regular fa-circle-down"></i>
+      </div>
+    </div>
+    <!-- sel_travel_consultant_banner end -->
+    
+    <!-- sel_travel_consultant -->
+    <div class="sel_travel_consultant">
+
+      <div class="self_or_random">
+        <div class="self">自定義</div>
+        <div class="random"><a href="">隨機</a></div>
+      </div>
+
+      <div id="app"> <!-- HTML DOM template -->    
+          <div class="self_consultant">
+              <button @click="content='all'" :class="{ 'self_consultant_all': content === 'all', 'self_consultant_unall': content !== 'all' }">全部</button>
+              <button @click="content='boy'" :class="{ 'self_consultant_boy': content === 'boy', 'self_consultant_unboy': content !== 'boy' }">男性</button>
+              <button @click="content='girl'" :class="{ 'self_consultant_girl': content === 'girl', 'self_consultant_ungirl': content !== 'girl' }">女性</button>
+          </div> 
+          <div class="container">                
+              <component :is="content"></component>
+
+              <div class="all_wrapper">
+                <!-- <template v-for="header in headers"> -->
+                <template v-for="(header, index) in headers">
+                    <div class="consultant_card">
+                      <ul>
+                          <li>
+                              <input type="checkbox" name="" id="button_id">
+                              <a href=""><img src="../assets/image/consultant_card.png" alt=""></a>
+                              <h1>裴大尼 </h1>
+                              <h2>5年經驗登山嚮導</h2>
+                              <!-- <i class="fa-regular fa-solid fa-heart"></i> -->
+                              <!-- <i class="fa-regular fa-heart" :class="{ 'fa-solid': isFavorite(header) }" @click="toggleFavorite(header)"></i> -->
+                              <i class="fa-regular fa-heart" :class="{ 'fa-solid': isFavorite(index) }" @click="toggleFavorite(index)"></i>
+                              <!-- <span v-if="isFavorite(header)">已收藏</span> -->
+                              <!-- <i class="fa-solid fa-heart"></i> -->
+                          </li>
+                      </ul>
+                    </div>                  
+                </template>
+              </div>
+
+              <div class="page_number">
+                <a href=""><i class="fa-solid fa-angle-left"></i></a> 
+                <a href="#">1</a>
+                <a href="#">2</a>
+                <a href="#">3</a>
+                <a href="#"><i class="fa-solid fa-angle-right"></i></a>
+              </div>
+              
+          </div>       
+      </div>
+
+    </div>
+    <!-- sel_travel_consultant end-->
+
+
+    <div class="sel_travel_consultant_button">
+      <div class="container">
+          <button class="outline_btn_orange">重新選擇地區</button>
+          <button class="btn_orange">開始預約</button>
+      </div>
+    </div>
+</template>
+
+<script>
+// import Slick from 'slick-carousel'
+
+export default {
+  name: "SelTravelConsultant",
+  data() {
+    return {
+        content: 'all',
+        headers: ['','','','','','','','','','','','','','','',''],
+        favorites: [],
+    }
+  },
+  methods: {
+        isFavorite(index) {
+            return this.favorites.includes(index)
+        },
+        toggleFavorite(index) {
+            if (this.isFavorite(index)) {
+            const favoriteIndex = this.favorites.indexOf(index)
+            this.favorites.splice(favoriteIndex, 1)
+            } else {
+            this.favorites.push(index)
+            }
+        },
+  },
+  
+  
+
+  
+
+};
+</script>
+
+<style lang="scss">
+@import "../assets/tgd104-sass/new_style.scss";
+</style>
---
+++ vue-lessons/src/views/Travel.vue
@@ -0,0 +1,175 @@
+<template>
+  <!-- banner  -->
+    <div class="travel_banner">
+        <div class="container">
+            <div class="left_block">
+                <h1>陪你</h1><span>旅行。</span>
+                <h2>比起走了多少公里<br>
+                    一路上結交了多少朋友是衡量一趟旅行更好的方式
+                </h2>
+                <img src="../assets/image/travel/travel_banner_people.png" alt="">
+            </div>
+         
+            <div class="right_block">
+                <img src="../assets/image/travel/travel_banner_person.png" alt="">
+            </div>
+        </div>
+    </div>
+    <!-- banner  end -->
+
+    <!-- travel_member -->
+    <section class="travel_member">
+        <div class="container">
+            <h1>陪你旅行的成員</h1>
+            <div class="autoplay">
+                <div class="card">
+                    <i class="fa-solid fa-star"></i>
+                    <p>4.5 (211)</p>
+                    <img src="../assets/image/travel/travel_member_1.png" alt="">
+                    <h2>徐嘉宏</h2>
+                    <h3>登山</h3>
+                </div>
+
+                <div class="card">
+                    <i class="fa-solid fa-star"></i>
+                    <p>4.5 (211)</p>
+                    <img src="../assets/image/travel/travel_member_1.png" alt="">
+                    <h2>徐嘉宏</h2>
+                    <h3>登山</h3>
+                </div>
+
+                <div class="card">
+                    <i class="fa-solid fa-star"></i>
+                    <p>4.5 (211)</p>
+                    <img src="../assets/image/travel/travel_member_1.png" alt="">
+                    <h2>徐嘉宏</h2>
+                    <h3>登山</h3>
+                </div>
+
+                <div class="card">
+                    <i class="fa-solid fa-star"></i>
+                    <p>4.5 (211)</p>
+                    <img src="../assets/image/travel/travel_member_1.png" alt="">
+                    <h2>徐嘉宏</h2>
+                    <h3>登山</h3>
+                </div>
+
+                <div class="card">
+                    <i class="fa-solid fa-star"></i>
+                    <p>4.5 (211)</p>
+                    <img src="../assets/image/travel/travel_member_1.png" alt="">
+                    <h2>徐嘉宏</h2>
+                    <h3>登山</h3>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <!-- travel_member end -->
+
+    <!-- travel_star -->
+    <section class="travel_star">
+        <div class="container">
+            <h1>「旅」獲好評！</h1>
+            <div class="travel_comment_wrapper">
+
+                <div class="travel_comments">
+
+                    <div class="travel_comment">
+                        <h2>a**********1</h2>
+                        <p>第一次使用陪你找到登山旅伴，體驗非常好! 下次再試試騎單車或潛水:]讚啦~~~~~~ 而且老師好幽默風趣!
+                            讚讚讚讚讚讚讚讚讚*10000 !</p>
+                        <h3>2023-02-18 11:58</h3>
+                    </div>
+                    <div class="travel_consultant_wrapper">
+                        <img src="../assets/image/travel/travel_consultant_1.png" alt="">
+                        <div class="travel_consultant">
+                            <h4>徐阿宏</h4>
+                            <i class="fa-solid fa-star"></i>
+                            <p>4.3 (158)</p>
+                            <h5>登山</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="travel_comments">
+                    <div class="travel_comment">
+                        <h2>a**********1</h2>
+                        <p>第一次使用陪你找到登山旅伴，體驗非常好! 下次再試試騎單車或潛水:]讚啦~~~~~~ 而且老師好幽默風趣!
+                            讚讚讚讚讚讚讚讚讚*10000 !</p>
+                        <h3>2023-02-18 11:58</h3>
+                    </div>
+                    <div class="travel_consultant_wrapper">
+                        <img src="../assets/image/travel/travel_consultant_1.png" alt="">
+                        <div class="travel_consultant">
+                            <h4>徐阿宏</h4>
+                            <i class="fa-solid fa-star"></i>
+                            <p>4.3 (158)</p>
+                            <h5>登山</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="travel_comments">
+                    <div class="travel_comment">
+                        <h2>a**********1</h2>
+                        <p>第一次使用陪你找到登山旅伴，體驗非常好! 下次再試試騎單車或潛水:]讚啦~~~~~~ 而且老師好幽默風趣!
+                            讚讚讚讚讚讚讚讚讚*10000 !</p>
+                        <h3>2023-02-18 11:58</h3>
+                    </div>
+                    <div class="travel_consultant_wrapper">
+                        <img src="../assets/image/travel/travel_consultant_1.png" alt="">
+                        <div class="travel_consultant">
+                            <h4>徐阿宏</h4>
+                            <i class="fa-solid fa-star"></i>
+                            <p>4.3 (158)</p>
+                            <h5>登山</h5>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!-- travel_star end -->
+
+    <!-- travel_button -->
+    <div class="travel_button">
+        <div class="container">
+            <div class="travel_button_hike">
+                <img src="../assets/image/travel/travel_button_hike.png" alt="">
+                <a href="/TravelMap"><button class="hike btn_orange">陪你登山</button></a> 
+            </div>
+            <div class="travel_button_bike">
+                <img src="../assets/image/travel/travel_button_bike.png" alt="">
+                <a href="/TravelMap"><button class="bike btn_orange">陪你單車</button></a>
+            </div>
+            <div class="travel_button_swim">
+                <img src="../assets/image/travel/travel_button_swim.png" alt="">
+                <a href="/TravelMap"><button class="swim btn_orange">陪你潛水</button></a>
+            </div>
+        </div>
+    </div>
+    <!-- travel_button end -->
+</template>
+
+<script>
+// import Slick from 'slick-carousel'
+
+export default {
+  name: "Travel",
+  data() {
+    return {
+   
+    };
+  },
+
+  
+
+};
+</script>
+
+<style lang="scss">
+@import "../assets/tgd104-sass/new_style.scss";
+</style>
