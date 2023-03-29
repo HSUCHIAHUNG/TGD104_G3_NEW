370eb34568ff2858bc822beb1999ee31319c5faf
pinyu23 <kazy7852@gmail.com>
2023-03-29 11:01:53 +0800

    品諭 03/29 11:01 footer & header

---
+++ vue-lessons/src/components/VFooter.vue
@@ -0,0 +1,57 @@
+<template>
+    <footer>
+        <div class="footer_inner">
+
+            <div class="footer_content container">
+                <div>
+                    <a href="index.html">
+                    <img src="../../../image/logo_footer.png" alt="陪你" />
+                    </a>
+                </div>
+                <div class="footer_content_word">
+                    <div>
+                    <p class="title">聯絡我們</p>
+                    <p>服務專線：(02)2712-0589</p>
+                    <p>服務信箱：accompanyyou@gmail.com</p>
+                    <p>營業時間：週一 ~ 週五 8：00 ~ 17：00</p>
+                    </div>
+                    <div>
+                    <p class="title">關於我們</p>
+                    <p>找不到人陪你? 陪你陪你，讓你不再孤單!</p>
+                    <p>陪你旅行 不孤單的旅行，就從有陪你開始</p>
+                    <p>陪你學習 讓學習散撥至各地</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer_socialmedia">
+                <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                <a href="#"><i class="fa-brands fa-line"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-youtube"></i></a>
+            </div>
+
+            <hr/>
+
+            <p class="tibame">
+            本網站為緯育TibaMe前端工程師班第82期學員專題作品，本平台僅供學習、展示之用。<br/>
+            2023© All rights reserved
+            </p>
+        </div>
+    </footer>
+</template>
+
+
+<script>
+    export default {
+        name: "VFooter",
+        data() {
+            return {};
+        },
+    };
+</script>
+
+
+<style lang="scss">
+@import "../../../tgd104-sass/new_style.scss";
+</style>
