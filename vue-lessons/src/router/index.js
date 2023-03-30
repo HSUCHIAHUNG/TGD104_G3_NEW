d980c23d3f1ff07150b987517fec8792593a03d5
HSUCHIAHUNG <h0989541162@gmail.com>
2023-03-30 18:18:22 +0800

    no message

--- vue-lessons/src/router/index.js
+++ vue-lessons/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import MemberFav from "../views/MemberFav.vue";
 
+
 const routes = [
   {
     path: "/",
