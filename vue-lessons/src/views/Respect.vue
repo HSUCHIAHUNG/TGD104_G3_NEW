288867e4130d53fca2395709592349f96604a866
HSUCHIAHUNG <h0989541162@gmail.com>
2023-04-06 21:42:41 +0800

    no message

--- vue-lessons/src/views/Respect.vue
+++ vue-lessons/src/views/Respect.vue
@@ -4,7 +4,7 @@
     <section class="about_banner">
         <div class="about_banner_container container">
             <div class="about_banner_left">
-                <h1  class="content_h1 test_text"><span class="content_span"></span></h1>
+                <h1 class="content_h1">陪你的故事緣起於,<span class="content_span">日本</span>這要從一個退休的大叔故事 ,開始說起... </h1>
             </div>
             <div class="about_banner_right"></div>
         </div>
@@ -16,7 +16,7 @@
         <div class="about_story1_container container">
             <div class="about_story1_left"></div>
             <div class="about_story1_right">
-                <h1 class="content_h1 typing" ><span class="content_span"></span><span class="content_span"></span></h1>
+                <h1 class="content_h1 typing">有一天<span class="content_span">大叔</span>走在人來人往的街道上偶然聽見<span class="content_span">女大學生</span>談話說道「大叔好噁心」! </h1>
                 <div class="about_story1_right_img"></div>
             </div>
         </div>
@@ -81,45 +81,45 @@ export default {
   },
   
   methods: {
-    handleScroll() {
-      const targetElement = this.$refs.target; // 取得參考名稱為target的DOM元素
-      const rect = targetElement.getBoundingClientRect(); // 取得物件位置資訊
-      const elementTop = rect.top;
+    // handleScroll() {
+    //   const targetElement = this.$refs.target; // 取得參考名稱為target的DOM元素
+    //   const rect = targetElement.getBoundingClientRect(); // 取得物件位置資訊
+    //   const elementTop = rect.top;
 
-      if (elementTop <= window.innerHeight) {
-        // 當物件頂端位置小於等於視窗高度時，表示已觸碰到頂端
-        console.log(elementTop);
-      }
-    },
-    handleResize() {
-      this.handleScroll(); // 視窗大小改變時重新計算目標物件位置資訊
-    }
+    //   if (elementTop <= window.innerHeight) {
+    //     // 當物件頂端位置小於等於視窗高度時，表示已觸碰到頂端
+    //     console.log(elementTop);
+    //   }
+    // },
+    // handleResize() {
+    //   this.handleScroll(); // 視窗大小改變時重新計算目標物件位置資訊
+    // }
   },
-  destroyed() {
-    window.removeEventListener('scroll', this.handleScroll); // 移除滾動事件監聽
-    window.removeEventListener('resize', this.handleResize); // 移除視窗縮放事件監聽
-  },
+  // destroyed() {
+  //   window.removeEventListener('scroll', this.handleScroll); // 移除滾動事件監聽
+  //   window.removeEventListener('resize', this.handleResize); // 移除視窗縮放事件監聽
+  // },
   
    mounted() {
-    window.addEventListener('scroll', this.handleScroll); // 監聽滾動事件
-    window.addEventListener('resize', this.handleResize); // 監聽視窗縮放事件
+    // window.addEventListener('scroll', this.handleScroll); // 監聽滾動事件
+    // window.addEventListener('resize', this.handleResize); // 監聽視窗縮放事件
     
-    const options = {
-      strings: ['陪你的故事緣起於日本,這要從一個退休大叔的,故事開始說起...'],
-      typeSpeed: 100,
-      showCursor : false,
-      loop : true
-    };
+    // const options = {
+    //   strings: ['陪你的故事緣起於日本,這要從一個退休大叔的,故事開始說起...'],
+    //   typeSpeed: 100,
+    //   showCursor : false,
+    //   loop : true
+    // };
 
-    const testtx = {
-      strings: ['陪你的故事緣起於日本,這要從一個退休大叔的,故事開始說起...'],
-      typeSpeed: 100,
-      showCursor : false,
-      loop : true
-    };
+    // const testtx = {
+    //   strings: ['陪你的故事緣起於日本,這要從一個退休大叔的,故事開始說起...'],
+    //   typeSpeed: 100,
+    //   showCursor : false,
+    //   loop : true
+    // };
 
-    new Typed('.typing', options);
-    new Typed('.test_text', testtx);
+    // new Typed('.typing', options);
+    // new Typed('.test_text', testtx);
   },
   
 };
