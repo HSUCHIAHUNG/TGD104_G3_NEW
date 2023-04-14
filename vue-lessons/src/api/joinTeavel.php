7ba8cfcbec7048a0317cb3a30b4b37d20af12f7b
HSUCHIAHUNG <h0989541162@gmail.com>
2023-04-14 19:48:49 +0800

    嘉宏

--- vue-lessons/src/api/joinTeavel.php
+++ vue-lessons/src/api/joinTeavel.php
@@ -1,13 +1,12 @@
 <?php
 
-
-    
+require_once ("conn.php");
 
 //MySQL相關資訊
-$db_host = "127.0.0.1";
-$db_user = "root";
-$db_pass = "password";
-$db_select = "G3_TEAM";
+// $db_host = "127.0.0.1";
+// $db_user = "root";
+// $db_pass = "password";
+// $db_select = "G3_TEAM";
 
 // $db_host = "127.0.0.1";
 // $db_user = "tibamefe_since2021";
@@ -15,10 +14,10 @@ $db_select = "G3_TEAM";
 // $db_select = "tibamefe_tgd104g3";
 
 //建立資料庫連線物件
-$dsn = "mysql:host=".$db_host.";dbname=".$db_select.";charset=utf8";
+// $dsn = "mysql:host=".$db_host.";dbname=".$db_select.";charset=utf8";
 
 //建立PDO物件，並放入指定的相關資料
-$pdo = new PDO($dsn, $db_user, $db_pass);
+// $pdo = new PDO($dsn, $db_user, $db_pass);
 
 
 //方法二: 放進變數
