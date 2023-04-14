ffbf8a382f1b66b6f2dbc77ed09f37652f7e801f
pinyu23 <kazy7852@gmail.com>
2023-04-14 19:30:13 +0800

    4/14 品諭 7:30

--- vue-lessons/src/views/Music.vue
+++ vue-lessons/src/views/Music.vue
@@ -75,24 +75,33 @@
                 </li>
             </ol>
           <!-- 顧問卡 -->
+<!-- 
+          <div>
+            <h1 style="height: 20px;">p123ffffffffffffffffffffffffffffff <br>ddfdfdgdffghhyr <br>dfdgoihoirejgoirejgoirejgoire <br>fdgpgjepigjrepogjreg</h1>
+          </div> -->
+          <div v-for="card in study_cards">
+            <h1 style="height: 20px;">{{card.about_title}}</h1>
+          </div>
+
+
           <div class="product-selection">
             <template v-for="product in products" :key="product.id" >
                 <div v-if="current ==='all' || current === product.lesson"> 
                   <div class="product">
                     <router-link to="/LearningGallery" target="_blank">
                       <div class="product-image">
-                        <img :src="product.image" :alt="product.name">
+                        <img :src="product.image" :alt="product.c_nickname">
                       </div>
                     </router-link>
                       <div class="product-details">
                         <router-link to="/LearningGallery" target="_blank">
-                          <h3>{{ product.name }}</h3>
-                          <p>{{ product.description }}</p>
+                          <h3>{{ product.c_nickname }}</h3>
+                          <p>{{ product.s_title }}</p>
                         </router-link>
                         <div class="product-actions">
                           <button :class="{ 'liked': product.liked }" @click="toggleLike(product)"><i class="fas fa-heart"></i></button>
                           <button v-if="selectedProductId === product.id" class="selected">已選擇 <i class="fa-solid fa-basket-shopping"></i></button>
-                          <button v-else @click="selectProduct(product)">選擇</button>
+                          <button v-else @click="selectProduct(product);">選擇</button>
                         </div>
                       </div>
                     </div>
@@ -100,11 +109,15 @@
             </template>
           </div>
 
+          
+
           <!-- 顧問卡end -->
           <div class="learning_go">
-            <router-link to="/LearningCalendar"><button class="btn_orange">開始預約</button></router-link>
+            <router-link to="/LearningCalendar"><button class="btn_orange" @click="booking">開始預約</button></router-link>
           </div>
         </div>
+
+
         <!-- 右邊end -->
       <!-- </div>                 -->
     <!-- </div>    -->
@@ -115,6 +128,7 @@
   <script>
     // import VFooter from "../components/VFooter.vue";
     import VHeader from "../components/VHeader.vue";
+    import $ from "jquery";
     export default {
       components: {
         // VFooter,
@@ -122,247 +136,218 @@
       },
       data() {
         return {
+          study_cards:[],
           selectedProductId: null,
           current:'all',
           products: [ 
             {
-              id: 1,
-              name: '鐵人',
-              description: '學烏克麗麗，輕鬆享受甜美音色！',
+              id: 1, // consultant
+              c_nickname: '鐵人', // consultant
+              s_title: '學烏克麗麗，輕鬆享受甜美音色！', // about_consultant
               image: require('../assets/image/consultant02.png'),
-              liked: false,
-              lesson: '烏克麗麗'
+              lesson: '烏克麗麗' // about_consultant
             },
             {
               id: 2,
-              name: '小甜甜',
-              description: '優秀老師帶你成就鋼琴夢想',
+              c_nickname: '小甜甜',
+              s_title: '優秀老師帶你成就鋼琴夢想',
               image: require('../assets/image/learning/music001.png'),
-              liked: false,
               lesson: '鋼琴'
             },
             {
               id: 3,
-              name: '鐵娘子',
-              description: '讓老師教你，如何把爵士鼓融入生活！',
+              c_nickname: '鐵娘子',
+              s_title: '讓老師教你，如何把爵士鼓融入生活！',
               image: require('../assets/image/learning/teacher_009.png'),
-              liked: false,
               lesson: '爵士鼓'
             },
             {
               id: 4,
-              name: '金剛不壞',
-              description: '跟著老師學習，讓你的鋼琴技巧更上一層樓！',
+              c_nickname: '金剛不壞',
+              s_title: '跟著老師學習，讓你的鋼琴技巧更上一層樓！',
               image: require('../assets/image/learning/music001.png'),
-              liked: false,
               lesson: '鋼琴'
             },
             {
               id: 5,
-              name: '鐵娘子',
-              description: '學吉他，彈出屬於你的音樂風格！',
+              c_nickname: '鐵娘子',
+              s_title: '學吉他，彈出屬於你的音樂風格！',
               image: require('../assets/image/learning/music004.png'),
-              liked: false,
               lesson: '吉他'
             },
             {
               id: 6,
-              name: '肥皂泡',
-              description: '學烏克麗麗，輕鬆享受甜美音色！',
+              c_nickname: '肥皂泡',
+              s_title: '學烏克麗麗，輕鬆享受甜美音色！',
               image: require('../assets/image/consultant02.png'),
-              liked: false,
               lesson: '烏克麗麗'
             },
             {
               id: 7,
-              name: '鐵人',
-              description: '優秀老師帶你成就鋼琴夢想',
+              c_nickname: '鐵人',
+              s_title: '優秀老師帶你成就鋼琴夢想',
               image: require('../assets/image/learning/music001.png'),
-              liked: false,
               lesson: '鋼琴'
             },
             {
               id: 8,
-              name: '小甜甜',
-              description: '學習爵士鼓，體驗獨特的節奏感！',
+              c_nickname: '小甜甜',
+              s_title: '學習爵士鼓，體驗獨特的節奏感！',
               image: require('../assets/image/learning/teacher_009.png'),
-              liked: false,
               lesson: '爵士鼓'
             },
             {
               id: 9,
-              name: '大嘴巴',
-              description: '鋼琴大師為你開啟音樂之門！',
+              c_nickname: '大嘴巴',
+              s_title: '鋼琴大師為你開啟音樂之門！',
               image: require('../assets/image/learning/music001.png'),
-              liked: false,
               lesson: '鋼琴'
             },
             {
               id: 10,
-              name: '鐵娘子',
-              description: '學習爵士鼓，體驗獨特的節奏感！',
+              c_nickname: '鐵娘子',
+              s_title: '學習爵士鼓，體驗獨特的節奏感！',
               image: require('../assets/image/learning/teacher_009.png'),
-              liked: false,
               lesson: '爵士鼓'
             },
             {
               id: 11,
-              name: '金剛不壞',
-              description: '學烏克麗麗，輕鬆享受甜美音色！',
+              c_nickname: '金剛不壞',
+              s_title: '學烏克麗麗，輕鬆享受甜美音色！',
               image: require('../assets/image/consultant02.png'),
-              liked: false,
               lesson: '烏克麗麗'
             },
             {
               id: 12,
-              name: '大嘴巴',
-              description: '跟著老師探索吉他的神秘世界！',
+              c_nickname: '大嘴巴',
+              s_title: '跟著老師探索吉他的神秘世界！',
               image: require('../assets/image/learning/music005.png'),
-              liked: false,
               lesson: '吉他'
             },
             {
               id: 13,
-              name: '小霸王',
-              description: '優秀老師帶你成就爵士鼓夢想',
+              c_nickname: '小霸王',
+              s_title: '優秀老師帶你成就爵士鼓夢想',
               image: require('../assets/image/consultant02.png'),
-              liked: false,
               lesson: '爵士鼓'
             },
             {
               id: 14,
-              name: '老鷹眼',
-              description: '鋼琴大師為你開啟音樂之門！',
+              c_nickname: '老鷹眼',
+              s_title: '鋼琴大師為你開啟音樂之門！',
               image: require('../assets/image/learning/music001.png'),
-              liked: false,
               lesson: '鋼琴'
             },
             {
               id: 15,
-              name: '大嘴巴',
-              description: '學吉他，彈出屬於你的音樂風格！',
+              c_nickname: '大嘴巴',
+              s_title: '學吉他，彈出屬於你的音樂風格！',
               image: require('../assets/image/learning/music006.png'),
-              liked: false,
               lesson: '吉他'
             },
             {
               id: 16,
-              name: '肥皂泡',
-              description: '跟著老師探索烏克麗麗的神秘世界！',
+              c_nickname: '肥皂泡',
+              s_title: '跟著老師探索烏克麗麗的神秘世界！',
               image: require('../assets/image/consultant02.png'),
-              liked: false,
               lesson: '烏克麗麗'
             },
             {
               id: 17,
-              name: '鐵娘子',
-              description: '跟著老師探索鋼琴的神秘世界！',
+              c_nickname: '鐵娘子',
+              s_title: '跟著老師探索鋼琴的神秘世界！',
               image: require('../assets/image/learning/music003.png'),
-              liked: false,
               lesson: '鋼琴'
             },
             {
               id: 18,
-              name: '金剛不壞',
-              description: '學習爵士鼓，體驗獨特的節奏感！',
+              c_nickname: '金剛不壞',
+              s_title: '學習爵士鼓，體驗獨特的節奏感！',
               image: require('../assets/image/consultant02.png'),
-              liked: false,
               lesson: '爵士鼓'
             },
             {
               id: 19,
-              name: '老鷹眼',
-              description: '讓老師教你，如何把鋼琴融入生活！',
+              c_nickname: '老鷹眼',
+              s_title: '讓老師教你，如何把鋼琴融入生活！',
               image: require('../assets/image/learning/music003.png'),
-              liked: false,
               lesson: '鋼琴'
             },
             {
               id: 20,
-              name: '小甜甜',
-              description: '學習爵士鼓，體驗獨特的節奏感！',
+              c_nickname: '小甜甜',
+              s_title: '學習爵士鼓，體驗獨特的節奏感！',
               image: require('../assets/image/consultant02.png'),
-              liked: false,
               lesson: '爵士鼓'
             },
             {
               id: 21,
-              name: '花心大少',
-              description: '學烏克麗麗，輕鬆享受甜美音色！',
+              c_nickname: '花心大少',
+              s_title: '學烏克麗麗，輕鬆享受甜美音色！',
               image: require('../assets/image/consultant02.png'),
-              liked: false,
               lesson: '烏克麗麗'
             },
             {
               id: 22,
-              name: '肥皂泡',
-              description: '學吉他，彈出屬於你的音樂風格！',
+              c_nickname: '肥皂泡',
+              s_title: '學吉他，彈出屬於你的音樂風格！',
               image: require('../assets/image/learning/music002.png'),
-              liked: false,
               lesson: '吉他'
             },
             {
               id: 23,
-              name: '小霸王',
-              description: '跟著老師學習，讓你的爵士鼓技巧更上一層樓！',
+              c_nickname: '小霸王',
+              s_title: '跟著老師學習，讓你的爵士鼓技巧更上一層樓！',
               image: require('../assets/image/consultant02.png'),
-              liked: false,
               lesson: '爵士鼓'
             },
             {
               id: 24,
-              name: '花心大少',
-              description: '鋼琴大師為你開啟音樂之門！',
+              c_nickname: '花心大少',
+              s_title: '鋼琴大師為你開啟音樂之門！',
               image: require('../assets/image/learning/music003.png'),
-              liked: false,
               lesson: '鋼琴'
             },
             {
               id: 25,
-              name: '鐵娘子',
-              description: '學吉他，彈出屬於你的音樂風格！',
+              c_nickname: '鐵娘子',
+              s_title: '學吉他，彈出屬於你的音樂風格！',
               image: require('../assets/image/learning/music007.png'),
-              liked: false,
               lesson: '吉他'
             },
             {
               id: 26,
-              name: '金剛不壞',
-              description: '讓老師教你，如何把烏克麗麗融入生活！',
+              c_nickname: '金剛不壞',
+              s_title: '讓老師教你，如何把烏克麗麗融入生活！',
               image: require('../assets/image/consultant02.png'),
-              liked: false,
               lesson: '烏克麗麗'
             },
             {
               id: 27,
-              name: '小霸王',
-              description: '鋼琴大師為你開啟音樂之門！',
+              c_nickname: '小霸王',
+              s_title: '鋼琴大師為你開啟音樂之門！',
               image: require('../assets/image/learning/music001.png'),
-              liked: false,
               lesson: '鋼琴'
             },
             {
               id: 28,
-              name: '花心大少',
-              description: '學習爵士鼓，體驗獨特的節奏感！',
+              c_nickname: '花心大少',
+              s_title: '學習爵士鼓，體驗獨特的節奏感！',
               image: require('../assets/image/consultant02.png'),
-              liked: false,
               lesson: '爵士鼓'
             },
             {
               id: 29,
-              name: '肥皂泡',
-              description: '跟著老師學習，讓你的鋼琴技巧更上一層樓！',
+              c_nickname: '肥皂泡',
+              s_title: '跟著老師學習，讓你的鋼琴技巧更上一層樓！',
               image: require('../assets/image/learning/music001.png'),
-              liked: false,
               lesson: '鋼琴'
             },
             {
               id: 30,
-              name: '老鷹眼',
-              description: '學習爵士鼓，體驗獨特的節奏感！',
+              c_nickname: '老鷹眼',
+              s_title: '學習爵士鼓，體驗獨特的節奏感！',
               image: require('../assets/image/consultant02.png'),
-              liked: false,
               lesson: '爵士鼓'
             },
           ],
@@ -372,11 +357,20 @@
       methods: {
         selectProduct(product) {
           this.selectedProductId = product.id
+          this.$cookies.set("cid",this.selectedProductId)
         },
         toggleLike(product) {
           product.liked = !product.liked
+        },
+
+
+        booking(){
+
         }
-      }
+      },
+      mounted() {
+        $.getJSON('http://localhost/TGD104_G3_NEW/vue-lessons/src/api/music_Select.php').then(response => this.study_cards = response)
+      },
     }
   </script>
   
