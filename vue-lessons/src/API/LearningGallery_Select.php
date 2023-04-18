393758d052640cc4f3be8c55d7edc8facfc4a706
pinyu23 <kazy7852@gmail.com>
2023-04-18 20:40:34 +0800

    品諭 04/18 08:40

---
+++ vue-lessons/src/API/LearningGallery_Select.php
@@ -0,0 +1,26 @@
+<?php
+
+       header("Access-Control-Allow-Origin: *");
+       header("Content-Type: application/json; charset=UTF-8");
+       header("Access-Control-Allow-Methods: GET, POST");
+       header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+       require_once ("conn.php");
+
+       $id = $_POST["Id"];
+
+       $sql = "SELECT *
+              FROM about_consultant abc
+              join consultant con
+              on abc.about_cid = con.id
+              where id = '$id';";
+
+
+       //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
+       $statement = $pdo->query($sql);
+
+       //抓出全部且依照順序封裝成一個二維陣列
+       $data = $statement->fetchAll();
+
+       echo json_encode($data);
+?>
