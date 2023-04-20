ac45404d2c8c0c6ee4087124d67378927c8d421b
HSUCHIAHUNG <h0989541162@gmail.com>
2023-04-20 16:34:16 +0800

    嘉宏

--- vue-lessons/src/API/memberInfo.php
+++ vue-lessons/src/API/memberInfo.php
@@ -7,16 +7,7 @@
 
 // 資料庫-------------------------------------------
 //MySQL相關資訊
-$db_host = "127.0.0.1";
-$db_user = "root";
-$db_pass = "9090yggep";
-$db_select = "G3_TEAM";
-
-//建立資料庫連線物件
-$dsn = "mysql:host=".$db_host.";dbname=".$db_select.";charset=utf8";
-
-//建立PDO物件，並放入指定的相關資料
-$pdo = new PDO($dsn, $db_user, $db_pass);
+require_once ("conn.php");
 // -------------------------------------------
 
 // 取得cookies 資料
