1cb3231fe3a2695aeccb39db29427e49ae96f8f2
HSUCHIAHUNG <h0989541162@gmail.com>
2023-04-29 14:51:54 +0800

    最新版

--- vue-lessons/src/config.js
+++ vue-lessons/src/config.js
@@ -10,9 +10,9 @@ export const API_URL = process.env.VUE_APP_API_URL || 'https://tibamef2e.com/tgd
 
 // 圖片路徑
 /* 本地 */
-// export const API_ARC =  'http://localhost/img/'
+export const API_ARC =  'http://localhost/img/'
 
 /* server */
-export const API_ARC =  'https://tibamef2e.com/tgd104/g3/img/'
+// export const API_ARC =  'https://tibamef2e.com/tgd104/g3/img/'
 
 
